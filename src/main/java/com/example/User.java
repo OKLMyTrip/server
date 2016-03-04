62d1af3d634d2ad845500a39719056f856c753e2
Togra <thomas.grassin@ynov.com>
2016-03-04 13:58:04 +0100

    add usercontroller add email and password to user model

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -11,19 +11,23 @@ public class User {
 
     private String firstName;
     private String lastName;
+    public String email;
+    public String password;
 
     public User() {}
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public User(String _firstName, String _lastName, String _email, String _password) {
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.email = _email;
+        this.password = _password;
     }
 
     @Override
     public String toString() {
         return String.format(
-                "Customer[id=%s, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
+                "Customer[id=%s, firstName='%s', lastName='%s', email='%s']",
+                id, firstName, lastName, email);
     }
 
 }
