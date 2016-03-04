f4280e4f4b1f9fabca8869be6adc62b597459ed0
PUJOL Johan <workfel@gmail.com>
2016-03-04 10:10:40 +0100

    init project

---
+++ src/main/java/com/example/HomeController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello-world")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String home() {
+        return "sisisi";
+    }
+
+}
